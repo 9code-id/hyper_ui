e267ccd1d584f845e6a529b81b32f652315c1ac3
denyocr93 <flutterlabz@gmail.com>
2024-08-13 21:30:12 +0700

    .

--- lib/module/snippet/cg_scaffold/_/scaffold_simple.dart
+++ lib/module/snippet/cg_scaffold/_/scaffold_simple.dart
@@ -22,6 +22,7 @@ class TemplateScaffoldSimple extends StatelessWidget {
       ),
     );
     //#END
+
     /*
     //#TEMPLATE appbar_white
     appBar: AppBar(
@@ -48,6 +49,65 @@ class TemplateScaffoldSimple extends StatelessWidget {
       ],
     ),
     //#END
+
+    //#TEMPLATE drawer
+    drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: const Text("Andrew Garfield"),
+              accountEmail: const Text("capek@ngoding.com"),
+              currentAccountPicture: const CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.blueGrey[900],
+              ),
+              otherAccountsPictures: const [
+                // CircleAvatar(
+                //   backgroundColor: Colors.white,
+                //   backgroundImage: NetworkImage(
+                //       "https://randomuser.me/api/portraits/women/74.jpg"),
+                // ),
+                // CircleAvatar(
+                //   backgroundColor: Colors.white,
+                //   backgroundImage: NetworkImage(
+                //       "https://randomuser.me/api/portraits/men/47.jpg"),
+                // ),
+              ],
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text("Home"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.code),
+              title: const Text("About"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.rule),
+              title: const Text("TOS"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.privacy_tip),
+              title: const Text("Privacy Policy"),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.logout,
+              ),
+              title: const Text("Logout"),
+              onTap: () {},
+            )
+          ],
+        ),
+      ),
+      //#END
     */
   }
 }
